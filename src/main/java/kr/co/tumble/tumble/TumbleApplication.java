9163247db0e1d2ec322f83047c0f8600cd9bfb93
palaoh <palaoh@plateer.com>
2023-07-28 08:50:34 +0900

    tumble start

---
+++ src/main/java/kr/co/tumble/tumble/TumbleApplication.java
@@ -0,0 +1,13 @@
+package kr.co.tumble.tumble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TumbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TumbleApplication.class, args);
+	}
+
+}

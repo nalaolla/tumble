9e25400d0eafc3755e873b334b62108a5a3541bf
palaoh <palaoh@plateer.com>
2023-07-28 09:43:08 +0900

    tumble start

--- src/main/java/kr/co/tumble/tumble/TumbleApplication.java
+++ src/main/java/kr/co/tumble/tumble/TumbleApplication.java
@@ -2,8 +2,9 @@ package kr.co.tumble.tumble;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = DataSourceAutoConfiguration.class)
 public class TumbleApplication {
 
 	public static void main(String[] args) {

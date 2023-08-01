df3d060c90d7eeceb6756a207377f22200b6e8a0
palaoh <palaoh@plateer.com>
2023-08-01 11:26:33 +0900

    20230801

---
+++ src/main/java/kr/co/tumble/tumble/main/MainController.java
@@ -0,0 +1,19 @@
+package kr.co.tumble.tumble.main;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * packageName    : kr.co.tumble.tumble.main
+ * fileName       : MainController
+ * author         : DANIEL
+ * date           : 2023-07-31
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2023-07-31        DANIEL       최초 생성
+ */
+
+@RestController
+public class MainController {
+}
